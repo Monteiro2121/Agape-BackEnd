561a9165969f20947bb8e70a5024193bcd321dca
Monteiro2121 <lipemontefarias@gmail.com>
2026-05-11 13:11:08 -0300

    correção em import no dashboardservice

--- src/main/java/com/aguape/JasperApplication.java
+++ src/main/java/com/aguape/JasperApplication.java
@@ -9,5 +9,4 @@ public class JasperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JasperApplication.class, args);
 	}
-
 }
--- src/main/java/com/aguape/service/DashboardService.java
+++ src/main/java/com/aguape/service/DashboardService.java
@@ -1,10 +1,12 @@
 package com.aguape.service;
 
 import com.aguape.dto.IndicadoresDTO;
-
+import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 
+@Service
 public class DashboardService {
+
     public IndicadoresDTO buscarIndicadores(LocalDate dataInicio, LocalDate dataFim) {
         return null;
     }
