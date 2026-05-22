dfb8bba18463d0dc59f85df4610b7c2ae7e4ef7c
Monteiro2121 <lipemontefarias@gmail.com>
2026-05-22 10:03:11 -0300

    adição do corsConfig

--- src/main/java/com/aguape/JasperApplication.java
+++ src/main/java/com/aguape/JasperApplication.java
@@ -1,7 +1,7 @@
 package com.aguape;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;gi
 
 @SpringBootApplication
 public class JasperApplication {
