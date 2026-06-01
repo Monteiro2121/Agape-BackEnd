0b6caa5788c2e5d4265e089b9b5dff8f07b30fc4
GuiRodriSil <rodriguesgui560@gmail.com>
2026-06-01 16:52:53 -0300

    Dockerfile add

--- src/main/java/com/aguape/JasperApplication.java
+++ src/main/java/com/aguape/JasperApplication.java
@@ -1,7 +1,7 @@
 package com.aguape;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;gi
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JasperApplication {
